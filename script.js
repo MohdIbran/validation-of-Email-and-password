5320f3afbb6db94939636d77ec6d94ce36ef3d34
Mohd Ibran <ronnyyadav100@gmail.com>
2025-08-27 22:47:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let form = document.querySelector("form");
+form.addEventListener("submit",function(dets) {
+   dets.preventDefault();
+   email.textContent = "";
+   password.textContent = "";
+   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/;
+   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/;
+   let checkmail = emailRegex.test(email.value);
+   let checkpassword = passwordRegex.test(password.value);
+   let output = true;
+   if (!checkmail) {
+      let a=document.querySelector("#emailerror");
+      a.style.display = "initial";
+      output = false;
+      
+   }
+   if (!checkpassword) {
+      let b= document.querySelector("#passworderror");
+      b.style.display = "initial";
+      output = false;
+      
+   }
+   if (output) {
+      let n = document.querySelector("#output");
+      n.style.display = "initial";
+      
+   }
+
+
+
+   
+})
